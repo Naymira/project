f7f1d85bcfec93a2d2226c32a710605fe36f7a93
pechenyka <innapechenyuk903@gmail.com>
2019-03-04 23:00:48 +0200

    first project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Код підключився");
+menu.onclick = function myFunction() {
+	var.x= document.getElementById("MyTopnav");
+
+	if(x.className === " topnav"){
+		x.className += "responsiv";
+	}else{
+		x.className = " topnav"
+	} 
+}
